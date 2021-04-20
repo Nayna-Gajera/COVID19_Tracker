0ca7b5f22936acd8dedaef56d255827894714692
Nayna Gajera <nayna.gajera21@gmail.com>
2021-04-19 21:36:58 -0700

    Updates

---
+++ precache-manifest.499575a0f80f52a0d50dc59ab7b3f69e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4299fe950dc0e63ca22b6df0a98916c4",
+    "url": "/COVID19_Tracker/index.html"
+  },
+  {
+    "revision": "d257401e1849b4c1ba97",
+    "url": "/COVID19_Tracker/static/css/main.669d26c0.chunk.css"
+  },
+  {
+    "revision": "d75ff34d029dcebd0dca",
+    "url": "/COVID19_Tracker/static/js/2.6dc9597a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19_Tracker/static/js/2.6dc9597a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d257401e1849b4c1ba97",
+    "url": "/COVID19_Tracker/static/js/main.a600dcd8.chunk.js"
+  },
+  {
+    "revision": "e0157e477afdde46e829",
+    "url": "/COVID19_Tracker/static/js/runtime-main.78a74a38.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19_Tracker/static/media/image.d7265326.png"
+  }
+]);
